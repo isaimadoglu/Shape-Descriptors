6d2cf7bedbdacab868010bdedafea7c54ca05dfc
İsa İmadoğlu <isaimadoglu@student.cu.edu.tr>
2018-11-24 17:02:24 +0300

    We have divided this function.
    We divided this function as fitnessFunction.m function and createDataset.m file.

--- fitnessFunction.m
+++ fitnessFunction.m
@@ -1,34 +1,5 @@
 function [basariOrani] = fitnessFunction(kromozom)
 
-%% section1
-% image_folder='C:\Users\isaim\Documents\MATLAB\images';
-% file_names = dir(fullfile(image_folder,'*.jpg'));
-% total_images = numel(file_names);
-% 
-% for n=1:total_images
-%    f=fullfile(image_folder, file_names(n).name);
-%     orj_im = imread(f);
-%    gray_im=rgb2gray(orj_im);
-% gray_im2=gray_im;
-% 
-% eta_mat = SI_moment(orj_im,gray_im2); 
-% hu_arr = Hu_Moments(eta_mat);
-% 
-% indisler=find(gray_im<165);
-% gray_im2(indisler)=0;
-% bin_im=imbinarize(gray_im2);
-% bin_im_removed = bwareaopen(bin_im,40);
-% bw_img=1.-bin_im_removed;
-% bw_img=imfill(bw_img,'holes');
-% %figure, imshow(bw_img);
-% 
-% 
-% stats = regionprops(bw_img,'Area','Eccentricity','Perimeter','EulerNumber');
-% vector(n,:)=[stats.Area stats.Eccentricity stats.Perimeter stats.EulerNumber];
-%    
-% end
-%% section2
-
 load etiketli_output2.mat
 one_indices = find(kromozom(:)==1);
 X=vector(1:19,one_indices);
