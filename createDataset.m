4e5bc1bc58d7a9761bb740631fb0441ccde4abc4
İsa İmadoğlu <isaimadoglu@student.cu.edu.tr>
2018-11-24 18:21:01 +0300

    Noises cleaned
    Our pictures don't have noises no longer. And we don't fill images anymore; because we want to analyze the Euler Numbers of the images.

--- createDataset.m
+++ createDataset.m
@@ -7,34 +7,41 @@ file_names = dir(fullfile(image_folder,'*.jpg'));
 total_images = numel(file_names);
 
 for n=1:total_images
-   f=fullfile(image_folder, file_names(n).name);
+    f=fullfile(image_folder, file_names(n).name);
     orj_im = imread(f);
-   gray_im=rgb2gray(orj_im);
-gray_im2=gray_im;
+%     figure, imshow(orj_im);
+    
+    gray_im=rgb2gray(orj_im);
+    gray_im2=gray_im;
+    
+    eta_mat = SI_moment(orj_im,gray_im2);
+    hu_arr = Hu_Moments(eta_mat);
+    
+    indisler=find(gray_im<165);
+    gray_im2(indisler)=0;
+    
+    bin_im=imbinarize(gray_im2);
+    bw_img=1.-bin_im;
+%     figure, imshow(bw_img); %figure
+    silme_sayisi= round(length(find(bw_img==1))/3);
+    bw_img = bwareaopen(bw_img,silme_sayisi);
 
-eta_mat = SI_moment(orj_im,gray_im2); 
-hu_arr = Hu_Moments(eta_mat);
-
-indisler=find(gray_im<165);
-gray_im2(indisler)=0;
-bin_im=imbinarize(gray_im2);
-bin_im_removed = bwareaopen(bin_im,40);
-bw_img=1.-bin_im_removed;
-bw_img=imfill(bw_img,'holes');
-%figure, imshow(bw_img);
-
-
-stats = regionprops(bw_img,'Area','Eccentricity','Perimeter','EulerNumber');
+%     bw_img=imfill(bw_img,'holes');
+%     figure, imshow(bw_img); %figure
+    
+    
+    stats = regionprops(bw_img,'Area','Eccentricity','Perimeter','EulerNumber');
     cevre= stats.Perimeter;
-alan = stats.Area;
+    alan = stats.Area;
     compactness = 4*pi*alan/cevre^2;
-vector(n,:)=[stats.Area stats.Eccentricity stats.Perimeter stats.EulerNumber hu_arr(7) compactness];
-   
+    disp([stats.EulerNumber]);
+    vector(n,:)=[stats.Area stats.Eccentricity stats.Perimeter stats.EulerNumber hu_arr(7) compactness];
+    
 end
 
 
 % bwo=imread('bird0.jpg');
 % mask=rgb2gray(bw);
-% 
-% eta_mat = SI_moment(bwo,mask); 
+%
+% eta_mat = SI_moment(bwo,mask);
 % hu_arr = Hu_Moments(eta_mat);
