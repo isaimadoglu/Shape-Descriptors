794a8ac16c7d5d2bb12a7b9b77de12251e52d4ab
İsa İmadoğlu <isaimadoglu@student.cu.edu.tr>
2018-11-24 23:41:07 +0300

    Update Main.m

--- Main.m
+++ Main.m
@@ -7,10 +7,16 @@ clearvars; clc;
 obj = @fitnessFunction;
 
 M = 10;   % number of chromosomes (candidate solutions)
-N = 4;    % number of genes(variables)
+N = 6;    % number of genes(variables)
 MaxGen = 4;
 Pc = 0.85;
 Pm = 0.01;
 Er = 0.2;
 
-[BestChrom] = GeneticAlgorithm (M, N, MaxGen, Pc, Pm, Er, obj)
+[BestChrom] = GeneticAlgorithm(M, N, MaxGen, Pc, Pm, Er, obj);
+
+disp('*******Best Chromosome******** ');
+disp(BestChrom.Gene);
+
+disp('Fitness= ');
+disp(BestChrom.fitness);
