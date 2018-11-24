8b1e7d6060a1111d24140ccb74f202823c2cc822
İsa İmadoğlu <isaimadoglu@student.cu.edu.tr>
2018-11-24 22:35:56 +0300

    Now we can see elite chromosomes.

--- elitism.m
+++ elitism.m
@@ -5,6 +5,11 @@ Elite_no= round(length(population.Chromosomes)*Er);
 [max_val, indx]= sort([population.Chromosomes(:).fitness], 'descend');
 
 for k=1: Elite_no
+    disp('Elite Chromosome= ');
+    disp(population.Chromosomes(indx(k)).Gene);
+    
+    disp('Fitness= ');
+    disp(population.Chromosomes(indx(k)).fitness);
     
     newPopulation.Chromosomes(k).Gene = population.Chromosomes(indx(k)).Gene;
     newPopulation.Chromosomes(k).fitness  = population.Chromosomes(indx(k)).fitness;
